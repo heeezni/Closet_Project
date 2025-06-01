9c8e9629f8e4f422074c25b842c0920e72c50700
heeezni <hjin3456@naver.com>
2025-06-01 19:37:55 +0900

    SampleClothes 형식 수정

--- dataInsert.js
+++ dataInsert.js
@@ -1,48 +1,20 @@
 function insertSampleClothes() {
     let sampleImages = [
         {
-            name: "슬리브리스",
+            clothesname: "슬리브리스",
             category: "상의",
             season: "여름",
             color: "화이트",
             path: "res/top.png"
         },
         {
-            name: "카고바지",
+            clothesname: "카고바지",
             category: "하의",
             season: "",
             color: "카키",
             path: "res/pants.png"
         }
     ];
-
-    for (let i = 0; i < sampleImages.length; i++) {
-        let item = sampleImages[i];
-
-        fetch(item.path)
-            .then(function(res) {
-                if (!res.ok) {
-                    throw new Error("이미지 로딩 실패");
-                }
-                return res.blob();
-            })
-            .then(function(blob) {
-                let file = new File([blob], item.name + ".png", { type: blob.type });
-
-                let clothesItem = {
-                    clothesname: item.name,
-                    category: item.category,
-                    season: item.season,
-                    color: item.color,
-                    image: file
-                };
-
-                let tx = db.transaction("clothes", "readwrite");
-                let store = tx.objectStore("clothes");
-                store.add(clothesItem);
-            })
-            .catch(function(error) {
-                console.error("삽입 실패:", error);
-            });
-    }
 }
+
+    
