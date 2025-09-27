07b87f3666daa7bdb562e6b70cdb7d53dd0f9c9b
heeezni <hjin3456@naver.com>
2025-09-27 10:54:24 +0900

    🐛 Fix : 날씨 API 관련 버그 수정 + 시크릿 키 관리

--- js/weather.js
+++ js/weather.js
@@ -1,22 +1,20 @@
 function weather(){
-    const API_KEY = "";
-    const CITY = "Seoul";
-    
-    fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${CITY}`)
-      .then(res => res.json())
-      .then(data => {
-        const temp = data.current.temp_c;
-        const condition = data.current.condition.text;
-        const icon = "https:" + data.current.condition.icon;
+  const CITY = "Seoul"
+
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CITY}&APPID=${APIKEY}&units=metric`)
+    .then((res) => res.json())
+    .then(data => {
+        console.log(data);
+        const temp = data.main.temp;
+        const condition = data.weather[0].description;
         
         document.getElementById("weather").innerHTML =
-          `${temp}°C, ${condition} <img src="${icon}">`;
+          `${temp}°C, ${condition}`;
       })
       .catch(err => {
         document.getElementById("weather").innerText = "날씨 정보를 가져올 수 없습니다.";
         console.error(err);
       });
-
 }
 
 
